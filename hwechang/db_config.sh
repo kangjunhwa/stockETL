9c4652ac7a2cff3331756b8bce1c65256b378917
정회창 <81364742+hwet-j@users.noreply.github.com>
2024-06-26 13:33:52 +0900

    hwet_config

---
+++ hwechang/db_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Database credentials
+DB_HOST="localhost"
+DB_NAME="stockdb"
+DB_USER="stockuser"
+DB_PASS="stockuser"
+DB_TABLE_TEMP="stock_temp"
+DB_TABLE="stock"
+DB_PORT="5432"
+DB_SCHEMA="public"
+
+# CSV 파일 경로
+CSV_FILE="/home/hwet/datalake/csv/stock.csv"
